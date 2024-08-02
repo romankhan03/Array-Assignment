2f6ec221f87fd6a0793ab58fc353856fb2ebd68b
romankhan03 <r.k03052029504@gmail.com>
2024-08-03 02:44:51 +0500

    array assignment

---
+++ app.js
@@ -0,0 +1,143 @@
+
+var string = ["string"]
+console.log(string);
+
+
+
+
+
+
+
+
+
+var number = [3456]
+console.log(number)
+
+
+
+
+
+
+
+
+
+
+
+var boolean = [true]
+console.log(boolean)
+
+
+
+
+
+
+
+
+
+
+
+
+
+var mixed = ["string" , 5 , false]
+console.log(mixed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var mobileNetworks = ["Zong" , "Jazz" , "Ufone" , "Telenor"]
+console.log(mobileNetworks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var topmovies = []
+topmovies.push("1) Avengers: Age of Ultron")
+topmovies.push("2) Spectre")
+topmovies.push("3) Jurassic World")
+console.log(topmovies)
+console.log(topmovies.length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var cars = ["Range Rover", "Mark X" , "Belta" , "Sonata"]
+console.log(cars[0])
+console.log(cars[3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var names = ["score of Ali is 225.Percentage:"]
+var scores = [225]
+var totalMarks = 500
+var ObtainedMarks = scores / totalMarks * 100
+var arr = names.concat(ObtainedMarks)
+console.log(arr + "%")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var sort = [320 , 120 , 560 , 430 , 100]
+sort.sort()
+console.log(sort)
